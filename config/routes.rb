0e950bd7489382eaec151f105535a9463e6b64fa
Kuroneko <ken.aster8@gmail.com>
2023-09-16 10:51:24 +0900

    deviseをセットアップ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  devise_for :users
   resources :books
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'books#index'
 end
