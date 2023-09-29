227a7d00871fa717b784b306e122cc929da0ac7d
Kuroneko <ken.aster8@gmail.com>
2023-09-29 12:03:35 +0900

    バリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,14 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one_attached :icon
+
+  has_one_attached :icon do |attachable|
+    attachable.variant :display, resize_to_limit: [100, 100]
+  end
+
+  validates :icon,
+            content_type: { in: %w[image/jpeg image/gif image/png],
+                            message: I18n.t('activerecord.errors.messages.icon.content_type', type: 'jpeg, gif, png') },
+            size: { less_than: 5.megabytes,
+                    message: I18n.t('activerecord.errors.messages.icon.size', limit: '5MB') }
 end
