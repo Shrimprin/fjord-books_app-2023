f732aaead4b3b5378f98ed61099a0e764e77b31c
Kuroneko <ken.aster8@gmail.com>
2023-11-19 16:57:43 +0900

    ハンズオン動画でユーザーモデルのテストと日報のシステムテストを作成

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -4,44 +4,56 @@ require 'application_system_test_case'
 
 class ReportsTest < ApplicationSystemTestCase
   setup do
-    @report = reports(:one)
+    @report = reports(:test_report)
+    visit root_url
+
+    fill_in User.human_attribute_name(:email), with: 'alice@example.com'
+    fill_in User.human_attribute_name(:password), with: 'password'
+    click_button I18n.t('devise.shared.links.sign_in')
+
+    assert_css 'h1', text: Book.model_name.human # CapybaraにTurbolinks処理完了を待たせる
   end
 
   test 'visiting the index' do
     visit reports_url
-    assert_selector 'h1', text: 'Reports'
+
+    assert_selector 'h1', text: I18n.t('views.common.title_index', name: Report.model_name.human)
   end
 
   test 'should create report' do
     visit reports_url
-    click_on 'New report'
+    click_on I18n.t('views.common.new', name: Report.model_name.human)
 
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Create Report'
+    fill_in Report.human_attribute_name(:content), with: '今日は晴れだった！'
+    fill_in Report.human_attribute_name(:title), with: '今日の天気'
+    click_on I18n.t('helpers.submit.create')
 
-    assert_text 'Report was successfully created'
-    click_on 'Back'
+    assert_text I18n.t('controllers.common.notice_create', name: Report.model_name.human)
+    assert_text '今日の天気'
+    assert_text '今日は晴れだった'
+    assert_text 'Alice'
+    click_on I18n.t('views.common.back', name: Report.model_name.human)
   end
 
   test 'should update Report' do
     visit report_url(@report)
-    click_on 'Edit this report', match: :first
+    click_on I18n.t('views.common.edit', name: Report.model_name.human), match: :first
 
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Update Report'
+    fill_in Report.human_attribute_name(:content), with: '今日は晴れのち曇り...'
+    fill_in Report.human_attribute_name(:title), with: '今日の天気（更新）'
+    click_on I18n.t('helpers.submit.update')
 
-    assert_text 'Report was successfully updated'
-    click_on 'Back'
+    assert_text I18n.t('controllers.common.notice_update', name: Report.model_name.human)
+    assert_text '今日の天気（更新）'
+    assert_text '今日は晴れのち曇り...'
+    click_on I18n.t('views.common.back', name: Report.model_name.human)
   end
 
   test 'should destroy Report' do
     visit report_url(@report)
-    click_on 'Destroy this report', match: :first
+    click_on I18n.t('views.common.destroy', name: Report.model_name.human), match: :first
 
-    assert_text 'Report was successfully destroyed'
+    assert_text I18n.t('controllers.common.notice_destroy', name: Report.model_name.human)
+    assert_selector 'h1', text: I18n.t('views.common.title_index', name: Report.model_name.human)
   end
 end
