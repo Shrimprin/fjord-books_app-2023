405904c983d4a4638f2831d3a6aff96abf3fda52
Kuroneko <ken.aster8@gmail.com>
2023-12-07 21:07:34 +0900

    email, nameをわかりやすい値に修正

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,12 +4,12 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test '#name_or_email should return email when user has not name' do
-    user = User.new(email: 'foo@example.com', name: '')
-    assert_equal 'foo@example.com', user.name_or_email
+    user = User.new(email: 'alice@example.com', name: '')
+    assert_equal 'alice@example.com', user.name_or_email
   end
 
   test '#name_or_email should return name when user has name' do
-    user = User.new(email: 'foo@example.com', name: 'Foo Bar')
-    assert_equal 'Foo Bar', user.name_or_email
+    user = User.new(email: 'alice@example.com', name: 'Alice')
+    assert_equal 'Alice', user.name_or_email
   end
 end
